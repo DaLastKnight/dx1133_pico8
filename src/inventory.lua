230019cabbab5508eb61964bee373ba62d59f15a
jeijeiliam <joonkiat0706@gmail.com>
2026-01-24 22:17:59 +0800

    updated inventory: - added checking for if there is current stock for item before using - added item prices and proper checking for players currency and items price

--- src/inventory.lua
+++ src/inventory.lua
@@ -1,7 +1,9 @@
 function init_inventory_currency()
     currency = 100
-    player_ingredients = {beef = 2, egg = 4, pizza = 2, coke = 3}
+    player_ingredients = {beef = 2, egg = 4}
     player_furnitures = {chair = 2, table = 1}
+    ingredients_price = {beef = 5, egg = 3}
+    furnitures_price = {chair = 20, table = 50}
 end
 
 -- updating and stuff
@@ -9,20 +11,27 @@ function update_currency(amount)
     currency += amount
 end
 
-function buy_ingredient(item, cost)
-    if currency >= cost then
-        add(player_ingredients, item, 1)
-        update_currency(-cost)
-        return true
-    else
-        return false
+function buy_ingredient(item)
+    for i=1,#player_ingredients do
+        if player_ingredients[i] == item then
+            if currency >= ingredients_price[item] then
+                player_ingredients[i] += 1
+                update_currency(-ingredients_price[item])
+                return true
+            else
+                return false
+            end
+        end
     end
 end
 
 function use_ingredient(item)
     for i=1,#player_ingredients do
         if player_ingredients[i] == item then
-            del(player_ingredients, item)
+            if player_ingredients[i] <= 0 then
+                return false
+            end
+            player_ingredients[i] -= 1
             return true
         end
     end
@@ -30,18 +39,25 @@ function use_ingredient(item)
 end
 
 function buy_furniture(item, cost)
-    if currency >= cost then
-        add(player_furnitures, item, 1)
-        update_currency(-cost)
-        return true
-    else
-        return false
+    for i=1,#player_furnitures do
+        if player_furnitures[i] == item then
+            if currency >= furnitures_price[item] then
+                player_furnitures[i] += 1
+                update_currency(-furnitures_price[item])
+                return true
+            else
+                return false
+            end
+        end
     end
 end
 
 function use_furniture(item)
     for i=1,#player_furnitures do
         if player_furnitures[i] == item then
+            if player_furnitures[i] <= 0 then
+                return false
+            end
             del(player_furnitures, item)
             return true
         end
