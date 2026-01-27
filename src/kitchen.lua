1ede52c7e0303eee0833e499708487893fe9a5cb
FongYiJie <fongyijie2008@gmail.com>
2026-01-28 01:34:41 +0800

    pop

--- src/kitchen.lua
+++ src/kitchen.lua
@@ -1,5 +1,5 @@
 kitchen = {}
-
+player = player
 function kitcheninit()
     kitchenX = 20
     kitchenY = 40
@@ -10,31 +10,38 @@ function kitcheninit()
 end
 
 function drawkitchen()
-    rectfill(0, 0, 128, 128, 12)
-    if (kitchenFaceDir == 0) then spr(0, kitchenX, kitchenY, 1, 1) end
-    if (kitchenFaceDir == 1) then spr(0, kitchenX, kitchenY, 1, 1) end
-    if (kitchenFaceDir == 2) then spr(0, kitchenX, kitchenY, 1, 1) end
-    if (kitchenFaceDir == 3) then spr(0, kitchenX, kitchenY, 1, 1) end
-    for i = 4, 6 do spr(i, 10, printspriteY * i - 40, 1, 1) end
-     printspriteY = 20
-    for i = 7, 9 do spr(i, 40, printspriteY * i - 100, 1, 1) end
-     printspriteY = 20
-    for i = 10, 12 do spr(i, 80, printspriteY * i - 160, 1, 1) end
-     printspriteY = 20
-    
+    rectfill(20, 20, 40, 40)
+    if (btn(5) and (player.x >= 14 and player.x <= 30) and (player.y >= 14 and player.y <= 30)) then
+      player.cooking = true
+      rectfill(8, 8, 120, 120, 12)
+      for i = 5, 7 do spr(i, 10, printspriteY * i - 60, 1, 1) end
+       printspriteY = 20
+      for i = 8, 10 do spr(i, 40, printspriteY * i - 120, 1, 1) end
+       printspriteY = 20
+      for i = 11, 13 do spr(i, 80, printspriteY * i - 180, 1, 1) end
+       printspriteY = 20
+      spr(50, kitchenX, kitchenY, 1, 1)
+    else 
+      player.cooking = false
+    end   
 end
 
 function movekitchen()
     
-    if (btn(0)) and choiceX == 3 then kitchenX = 50; choiceX = 2; kitchenFaceDir = 0 -- Go left
-    elseif (btn(0)) and choiceX == 2 then kitchenX = 20; choiceX = 1; kitchenFaceDir = 0 end
+    if (btn(0)) and choiceX == 3 then kitchenX = 50; choiceX = 2; -- Go left
+    elseif (btn(0)) and choiceX == 2 then kitchenX = 20; choiceX = 1; end
 
-    if (btn(1)) and choiceX == 1 then kitchenX = 50; choiceX = 2; kitchenFaceDir = 1  -- Go right
-    elseif (btn(1)) and choiceX == 2 then kitchenX = 90; choiceX = 3; kitchenFaceDir = 1 end
+    if (btn(1)) and choiceX == 1 then kitchenX = 50; choiceX = 2;  -- Go right
+    elseif (btn(1)) and choiceX == 2 then kitchenX = 90; choiceX = 3; end
 
-    if (btn(2)) and choiceY == 3 then kitchenY = 60; choiceY = 2; kitchenFaceDir = 2  -- Go up
-    elseif (btn(2)) and choiceY == 2 then kitchenY = 40; choiceY = 1; kitchenFaceDir = 2 end
+    if (btn(2)) and choiceY == 3 then kitchenY = 60; choiceY = 2;  -- Go up
+    elseif (btn(2)) and choiceY == 2 then kitchenY = 40; choiceY = 1; end
 
-    if (btn(3)) and choiceY == 1 then kitchenY = 60; choiceY = 2; kitchenFaceDir = 3 -- Go down
-    elseif (btn(3)) and choiceY == 2 then kitchenY = 80; choiceY = 3; kitchenFaceDir = 3 end
+    if (btn(3)) and choiceY == 1 then kitchenY = 60; choiceY = 2;-- Go down
+    elseif (btn(3)) and choiceY == 2 then kitchenY = 80; choiceY = 3; end
+
+    if (btn(5)) then 
+    draw_dialogue("You cooked the ingredient!") -- draw dialogue box but not working cause can't access drawText.lua
+    -- put into inventory but not implemented
+    end
 end
